6480b2c331f601121be4b996d0d7bb0c5e37f70e
unknown <contatogabrielcruzrodrigues@gmail.com>
2023-09-02 08:10:42 -0300

    created methods from connection database

--- app/src/main/java/util/connectionFactory.java
+++ app/src/main/java/util/connectionFactory.java
@@ -1,9 +1,32 @@
 
 package util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
 public class connectionFactory {
     public static final String DRIVER = "com.mysql.jdbc.Driver";
     public static final String URL = "jdbc:mysql://localhost:3306/todoApp";
     public static final String USER = "root";
     public static final String PASS = "1234";
+    
+    public static Connection getConnection() {
+        try {
+            Class.forName(DRIVER);
+            return DriverManager.getConnection(URL, USER, PASS);
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro na conexão com o banco de dados", ex);
+        }
+    }
+    
+    public static void closeConnection(Connection connection) {
+        try {
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception ex) {
+            throw new RuntimeException("Erro so fechar a conexão com o banco de dados.");
+        }
+    }
 }
